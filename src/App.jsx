6d0a7e26ac576d7baa9b999135d33319ddaed3c9
Kishore <sskrishkk411@gmail.com>
2025-01-05 13:12:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,33 @@ import MarkdownEditor from "./MarkdownEditor";
 const App = () => {
   return (
     <div style={{ padding: "20px" }}>
-      <h1 style={{
-        textAlign: "center",
-        color:"white"
-      }}>Markdown to PDF Converter</h1>
+      <h1
+        style={{
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        Markdown to PDF/HTML Converter
+      </h1>
+      <hr
+        style={{
+          color: "gray",
+          width:"50%"
+        }}
+      />
+      <p
+        style={{
+          width: "50%",
+          textAlign: "center",
+          margin: "auto",
+          color: "white",
+          lineHeight:"30px"
+        }}
+      >
+        {" "}
+        A simple and powerful React-based tool designed to help you build and export your CV in beautifully formatted
+        PDFs or HTML files directly from Markdown.
+      </p>
       <MarkdownEditor />
     </div>
   );
